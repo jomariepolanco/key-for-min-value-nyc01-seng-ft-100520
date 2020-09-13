782a550f20210830b8ac4a91ec9eced756323248
Jomarie Polanco <jomariepolanco@gmail.com>
2020-09-13 17:14:20 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,8 +5,8 @@ def key_for_min_value(name_hash)
   min_val = nil 
   i = 0 
   name_hash.each do |name, value|
-    if value < name_hash[i - 1] && value < min_val
-      min_val = value 
+    if min_val = 0 || value < min_val 
+      min_val = name 
     end 
     i += 1 
   end 
