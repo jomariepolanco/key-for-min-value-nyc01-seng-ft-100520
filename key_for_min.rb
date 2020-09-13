514dafae5f6836dcb8cc6277478ede2f8a22abbd
Jomarie Polanco <jomariepolanco@gmail.com>
2020-09-13 17:02:09 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,11 +4,11 @@
 def key_for_min_value(name_hash)
   min_key = ""
   name_hash.each do |name, value|
-    if value[0] < (value[1] && value[2])
+    if (value[0] < value[1]) && (value[0] < value[2])
       min_key = name[0] 
-    elsif value[1] < (value[0] && value [2])
+    elsif (value[1] < value[0]) && (value[1] < value [2])
       min_key = name[1]
-    elseif value[2] < (value[0] && value[1])
+    elseif (value[2] < value[0]) && (value[2] < value[1])
       min_key = name[2]
     else 
       min_key = nil 
